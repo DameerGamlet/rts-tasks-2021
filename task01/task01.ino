9cdb9939f682de2ffadf4058bdbdd729e3c8b85c
Damir <74682814+DameerGamlet@users.noreply.github.com>
2021-10-02 17:03:08 +0400

    Update task01.ino

--- task01/task01.ino
+++ task01/task01.ino
@@ -6,7 +6,7 @@
 #define led11 11
 
 long interval3 = 50000, interval5 = 100000, interval6 = 200000, interval9 = 400000, interval10 = 800000, interval11 = 1600000;
-int ledState3 = LOW, ledState5 = LOW, ledState6 = LOW, ledState9 = LOW, ledState10 = LOW, ledState11 = LOW;
+int ledState = LOW;
 unsigned long previousMillis3 = 0, previousMillis5 = 0, previousMillis6 = 0, previousMillis9 = 0, previousMillis10 = 0, previousMillis11 = 0;
 
 void setup() {
@@ -19,69 +19,64 @@ void setup() {
 }
 
 void loop() {
-  unsigned long currentMillis3 = micros();
-  if (currentMillis3 - previousMillis3 >= interval3) {
-    previousMillis3 = currentMillis3;
-    if (ledState3 == LOW) {
-      ledState3 = HIGH;
+  unsigned long currentMillis = micros();
+  if (currentMillis - previousMillis3 >= interval3) {
+    previousMillis3 = currentMillis;
+    if (ledState == LOW) {
+      ledState = HIGH;
     } else {
-      ledState3 = LOW;
+      ledState = LOW;
     }
-    digitalWrite(led3, ledState3);
+    digitalWrite(led3, ledState);
   }
 
-    unsigned long currentMillis5 = micros();
-  if (currentMillis5 - previousMillis5 >= interval5) {
-    previousMillis5 = currentMillis5;
-    if (ledState5 == LOW) {
-      ledState5 = HIGH;
+  if (currentMillis - previousMillis5 >= interval5) {
+    previousMillis5 = currentMillis;
+    if (ledState == LOW) {
+      ledState = HIGH;
     } else {
-      ledState5 = LOW;
+      ledState = LOW;
     }
-    digitalWrite(led5, ledState5);
+    digitalWrite(led5, ledState);
   }
 
-    unsigned long currentMillis6 = micros();
-  if (currentMillis6 - previousMillis6 >= interval6) {
-    previousMillis6 = currentMillis6;
-    if (ledState6 == LOW) {
-      ledState6 = HIGH;
+  if (currentMillis - previousMillis6 >= interval6) {
+    previousMillis6 = currentMillis;
+    if (ledState == LOW) {
+      ledState = HIGH;
     } else {
-      ledState6 = LOW;
+      ledState = LOW;
     }
-    digitalWrite(led6, ledState6);
+    digitalWrite(led6, ledState);
   }
 
-    unsigned long currentMillis9 = micros();
-  if (currentMillis9 - previousMillis9 >= interval9) {
-    previousMillis9 = currentMillis9;
-    if (ledState9 == LOW) {
-      ledState9 = HIGH;
+  if (currentMillis - previousMillis9 >= interval9) {
+    previousMillis9 = currentMillis;
+    if (ledState == LOW) {
+      ledState = HIGH;
     } else {
-      ledState9 = LOW;
+      ledState = LOW;
     }
-    digitalWrite(led9, ledState9);
+    digitalWrite(led9, ledState);
+  }
+  
+  if (currentMillis - previousMillis10 >= interval10) {
+    previousMillis10 = currentMillis;
+    if (ledState == LOW) {
+      ledState = HIGH;
+    } else {
+      ledState = LOW;
+    }
+    digitalWrite(led10, ledState);
   }
 
-    unsigned long currentMillis10 = micros();
-  if (currentMillis10 - previousMillis10 >= interval10) {
-    previousMillis10 = currentMillis10;
-    if (ledState10 == LOW) {
-      ledState10 = HIGH;
+  if (currentMillis - previousMillis11 >= interval11) {
+    previousMillis11 = currentMillis;
+    if (ledState == LOW) {
+      ledState = HIGH;
     } else {
-      ledState10 = LOW;
+      ledState = LOW;
     }
-    digitalWrite(led10, ledState10);
-  }
-
-    unsigned long currentMillis11 = micros();
-  if (currentMillis11 - previousMillis11 >= interval11) {
-    previousMillis11 = currentMillis11;
-    if (ledState11 == LOW) {
-      ledState11 = HIGH;
-    } else {
-      ledState11 = LOW;
-    }
-    digitalWrite(led11, ledState11);
+    digitalWrite(led11, ledState);
   }
 }
