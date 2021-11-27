50a33a5533b59b5ce20db05765e361e126280f8b
Damir <74682814+DameerGamlet@users.noreply.github.com>
2021-11-27 23:38:30 +0400

    init

--- task03/task03.ino
+++ task03/task03.ino
@@ -1,7 +1,33 @@
-void setup() {
+const int pinEnable = 8; 
+const int pinStep = 2;   
+const int pinDir = 5;    
 
+void setup() {
+  pinMode(pinEnable, OUTPUT);
+  pinMode(pinStep, OUTPUT);
+  pinMode(pinDir, OUTPUT);
+  digitalWrite(pinEnable, LOW);
 }
 
 void loop() {
+  digitalWrite(pinDir, HIGH);
+  for (int x = 0; x < 4000; x++) {
+    digitalWrite(pinStep, HIGH);
+    delayMicroseconds(50);
+    digitalWrite(pinStep, LOW);
+    delayMicroseconds(50);
+  }
+  delay(350);
 
+  digitalWrite(pinDir, LOW);
+  for (int x = 0; x < 4000; x++) {
+    digitalWrite(pinStep, HIGH);
+    delayMicroseconds(50);
+    digitalWrite(pinStep, LOW);
+    delayMicroseconds(50);
+  }
+  delay(350);
+  digitalWrite(pinEnable, HIGH);
+  delay(350);
+  digitalWrite(pinEnable, LOW);
 }
